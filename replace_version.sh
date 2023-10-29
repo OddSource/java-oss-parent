a44d418a817890294425c4ff375b7869213cdf33
Nick Williams <github@nicholaswilliams.net>
2023-10-28 21:00:29 -0500

    Prepare for release

--- replace_version.sh
+++ replace_version.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+# Copyright © 2010-2023 OddSource Code (license@oddsource.io)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 CURRENT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
 NEXT_VERSION=$1
